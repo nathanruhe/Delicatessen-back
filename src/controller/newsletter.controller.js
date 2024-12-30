6ca39157f6910529f997e315fc2a419a42305cfb
nathanruhe <nathan.ruhe@hotmail.com>
2024-12-30 23:36:47 +0100

    funcionalidad newsletter

--- src/controller/newsletter.controller.js
+++ src/controller/newsletter.controller.js
@@ -1,1 +1,32 @@
 const {pool} = require("../database");
+
+async function addNewsLetter (request, response) {
+    try {
+        let sql;
+        let params;
+        let respuesta;
+
+        sql = `SELECT * FROM newsletter WHERE email = ?`;
+        params = [request.body.email];
+
+        let [existe] = await pool.query(sql, params);
+
+        if (existe.length > 0) {
+            respuesta = {error: true, code: 200, message: "El email introducido ya existe"};
+        } else {
+            sql = `INSERT INTO newsletter (email)
+                    VALUES (?)`;
+            params = [request.body.email];
+
+            let [result] = await pool.query(sql, params);
+            respuesta = {error: false, code: 200, message: "Subscrito correctamente", data: result};
+        };
+
+        response.send(respuesta);
+
+    } catch (error) {
+        console.log(error);
+    };   
+};
+
+module.exports = {addNewsLetter};
--- src/routers/newsletter.router.js
+++ src/routers/newsletter.router.js
@@ -2,5 +2,6 @@ const { Router } = require("express");
 const router = Router();
 const newsletterCtrl = require("../controller/newsletter.controller");
 
+router.post("/newsletter", newsletterCtrl.addNewsLetter);
 
 module.exports = router;
