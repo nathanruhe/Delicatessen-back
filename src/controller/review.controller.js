3ff278f27a23d6d7ebc4c8c8e683861201cc3ed2
nathanruhe <nathan.ruhe@hotmail.com>
2024-12-31 01:06:38 +0100

    funcionalidad review

--- src/controller/review.controller.js
+++ src/controller/review.controller.js
@@ -1,1 +1,44 @@
 const {pool} = require("../database");
+
+async function getReview (request, response) {
+    try {
+        let sql;
+        let respuesta;
+
+        sql = `SELECT * FROM review`;
+
+        let [result] = await pool.query(sql);
+        respuesta = ({ error: false, codigo: 200, mensaje: "Reseñas obtenidas", dataReview: result });
+        
+        response.send(respuesta);
+
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+async function addReview (request, response) {
+    try {
+        let sql;
+        let params;
+        let respuesta;
+
+        sql = `INSERT INTO review (stars, name, message)
+                VALUES (?,?,?)`;
+        params = [
+            request.body.stars,
+            request.body.name,
+            request.body.message,
+        ];
+
+        let [result] = await pool.query(sql, params);
+        respuesta = {error: false, code: 200, message: "Valoración añadida correctamente", dataReview: result};
+
+        response.send(respuesta);
+
+    } catch (error) {
+        console.log(error);
+    };   
+};
+
+module.exports = {getReview, addReview};
--- src/routers/review.router.js
+++ src/routers/review.router.js
@@ -2,5 +2,7 @@ const { Router } = require("express");
 const router = Router();
 const reviewCtrl = require("../controller/review.controller");
 
+router.get("/review", reviewCtrl.getReview);
+router.post("/review", reviewCtrl.addReview);
 
 module.exports = router;
