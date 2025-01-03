88807969efce4f6710e22f2dcb3279a3e6281884
nathanruhe <nathan.ruhe@hotmail.com>
2025-01-03 13:32:22 +0100

    fix dashboard (events)

--- src/routers/event.router.js
+++ src/routers/event.router.js
@@ -5,6 +5,6 @@ const eventCtrl = require("../controller/event.controller");
 router.get("/events", eventCtrl.getEvent);
 router.post("/dashboard", eventCtrl.addEvent);
 router.put("/dashboard", eventCtrl.putEvent);
-router.delete("/dashboard", eventCtrl.deleteEvent);
+router.delete("/events", eventCtrl.deleteEvent);
 
 module.exports = router;
